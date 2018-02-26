31a4dc1a80373c22c3e24ccf7914f711c59829fd
Tamás <hencztamas95@gmail.com>
2018-02-26 06:38:17 -0800

    New JUnit test

--- train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
+++ train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
@@ -50,5 +50,14 @@ public class TrainSystemTest {
 		Assert.assertEquals(0, controller.getReferenceSpeed());
 	}
 
+	@Test
+	public void OverridingJoystickPositionToNegative_SetsReferenceSpeedToZero2() {
+		user.overrideJoystickPosition(4);
+		controller.followSpeed();
+		user.overrideJoystickPosition(-10);
+		controller.followSpeed();
+		Assert.assertEquals(0, controller.getReferenceSpeed());
+	}
+
 	
 }
